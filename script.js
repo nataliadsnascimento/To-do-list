be335e9697f9478903b73bfcb060f2a4f915a1d4
Natalia Nascimento <nataliasanntos161@gmail.com>
2026-02-09 18:37:53 -0300

    Checkbox das tarefas

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const addBtn = document.getElementById("addBtn");
 const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 
-
 addBtn.addEventListener('click', function() {
     const texto = taskInput.value.trim();
     
@@ -10,15 +9,26 @@ addBtn.addEventListener('click', function() {
         return;
     }
 
+    const leftDiv = document.createElement("div");
+    leftDiv.classList.add("task-left");
+
     const li = document.createElement('li');
     li.classList.add('task-item');
+
+    const checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.classList.add("task-checkbox");
 
     const span = document.createElement("span");
     span.innerText = texto;
     span.classList.add("task-text");
 
-    span.addEventListener('click', function() {
-        li.classList.toggle('completed');
+    checkbox.addEventListener('change', function() {
+        if (checkbox.checked) {
+            span.classList.add('completed');
+        } else {
+            span.classList.remove('completed');
+        }
     });
 
     const buttonsDiv = document.createElement("div");
@@ -45,25 +55,35 @@ addBtn.addEventListener('click', function() {
             input.type = "text";
             input.value = span.innerText;
             input.classList.add("edit-input");
+            
+            input.style.padding = "5px";
+            input.style.fontSize = "15px";
+            input.style.flex = "1";
 
-            li.replaceChild(input, span);
+            leftDiv.replaceChild(input, span);
             editBtn.innerText = "Salvar";
+            editBtn.style.backgroundColor = "#15ae2c";
         } else {
-            const input = li.querySelector(".edit-input");
+
+            const input = leftDiv.querySelector(".edit-input");
 
             span.innerText = input.value;
-            li.replaceChild(span, input);
+            
+            leftDiv.replaceChild(span, input);
             editBtn.innerText = "Editar";
+            editBtn.style.backgroundColor = "#f9cd1c"; 
         }
     });
 
     buttonsDiv.appendChild(editBtn);
     buttonsDiv.appendChild(deleteBtn);
 
-    li.appendChild(span);
+    leftDiv.appendChild(checkbox);
+    leftDiv.appendChild(span);
+    
+    li.appendChild(leftDiv);
     li.appendChild(buttonsDiv);
     taskList.appendChild(li);
 
     taskInput.value = "";
 });
-
